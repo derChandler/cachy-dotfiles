e0220e16cea32015cd0804436bef08dc204b88a4
derChandler <florian@anlauf.media>
2026-06-28 11:13:26 +0200

    feat(ghostty): add styles to ghostty

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -21,3 +21,15 @@ set --export PATH $BUN_INSTALL/bin $PATH
 
 # Added by Antigravity CLI installer
 set -gx PATH "/home/florian/.local/bin" $PATH
+
+# Cusor style block
+set -g fish_cursor_default block
+set -g fish_cursor_insert block
+set -g fish_cursor_replace_one block
+set -g fish_cursor_visual block
+
+# Paste with middle Mouse Button 
+function fish_user_key_bindings
+    bind --mode default \x1b\[M\" paste_from_clipboard
+    bind --mode insert \x1b\[M\" paste_from_clipboard
+end
